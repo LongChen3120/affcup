99e8f594d84dfc618379596124d7fc61004330ea
LongChen3120 <longa13k63@gmail.com>
2022-12-26 10:35:45 +0700

    thay đổi crontrigger

--- scr/main_crawl.py
+++ scr/main_crawl.py
@@ -31,23 +31,23 @@ def call_api_set_lich_tuong_thuat(time_start, doc):
     log_main.info(f"__payload:{payload}")
     try:
         res = requests.post(config_env.API_SCHEDULE_CRAWL, headers=config_env.HEADER, json=payload)
-        logging.warning(f"__response: {res.text}\nstart run crawl thuong thuat luc:{time_start_crawl},")
+        log_main.warning(f"__response: {res.text}\nstart run crawl thuong thuat luc:{time_start_crawl},")
     except:
-        logging.warning("exception: ", exc_info=True)
+        log_main.warning("exception: ", exc_info=True)
 
 
 def crawl_handler(id_match):
     check_job()
-    try:
-        crawl_lich_bxh.crawl()
-    except:
-        log_main.exception(f"message: exception when crawl lich", exc_info=True)
-    try:
-        crawl_detail.crawl2(id_match)
-    except:
-        log_main.exception(f"message: exception when crawl detail", exc_info=True)
-    finally:
-        db_handler.up_log()
+    # try:
+    #     crawl_lich_bxh.crawl()
+    # except:
+    #     log_main.exception(f"message: exception when crawl lich", exc_info=True)
+    # try:
+    #     crawl_detail.crawl2(id_match)
+    # except:
+    #     log_main.exception(f"message: exception when crawl detail", exc_info=True)
+    # finally:
+    #     db_handler.up_log()
 
 
 def check_job():
@@ -57,7 +57,7 @@ def check_job():
         log_main.info(f"check_time: {list_job[-1].next_run_time},\nlist match:{[i['_id'] for i in list_match]}")
         for match in list_match:
             time_start = func.convet_time_start(match['_source']['time'])
-            call_api_set_lich_tuong_thuat(time_start, match)
+            # call_api_set_lich_tuong_thuat(time_start, match)
             time_run = func.get_time_run(time_start, 5)
             add_job(match['_id'], time_run)
             list_job = scheduler.get_jobs()
@@ -65,9 +65,12 @@ def check_job():
 
 def add_job(id_match, time_run):
     log_main.info(f"next run at: {time_run}")
-    trigger = CronTrigger(year="*", month="*", day="*", hour=time_run.hour, minute=time_run.minute, second=time_run.second)
+    trigger = CronTrigger(year=time_run.year, month=time_run.month, day=time_run.day, hour=time_run.hour, minute=time_run.minute, second=time_run.second)
     scheduler.add_job(crawl_handler, trigger=trigger, args=[id_match], max_instances=10, name=f"job: {id_match}")
-    log_main.info(f"add job ok, list jobs:\n{scheduler.get_jobs()}")
+    log_main.info("list jobs:")
+    for job in scheduler.get_jobs():
+        log_main.info(job)
+    # log_main.info(f"add job ok, list jobs:\n{scheduler.get_jobs()}")
 
 
 def scheduler_run(list_match):
@@ -76,8 +79,8 @@ def scheduler_run(list_match):
     scheduler.start()
     for match in list_match:
         time_start = func.convet_time_start(match['_source']['time'])
-        call_api_set_lich_tuong_thuat(time_start, match)
-        time_run = func.get_time_run(time_start, 0)
+        # call_api_set_lich_tuong_thuat(time_start, match)
+        time_run = func.get_time_run(time_start, 5)
         add_job(match['_id'], time_run)
     
     while True:
